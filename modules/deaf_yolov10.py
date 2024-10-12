033e62895deb5b803ad7670321acae5b1d57f4ea
tondt <tondt@dyno.vn>
2024-10-12 14:53:18 +0700

    add

---
+++ modules/deaf_yolov10.py
@@ -0,0 +1,10 @@
+from ultralytics_v10 import YOLOv10
+model = YOLOv10('models/best_v10.pt')
+labels = model.names
+def predictDeaf(image):
+    result = model(image,verbose = False)[0]
+    cls = result.boxes.cls.cpu().detach().numpy()
+    boxes = result.boxes.xyxy.cpu().detach().numpy().astype('int')
+    if len(boxes)>0:
+        return labels.get(cls[0]), boxes[0]
+    return 'binh thuong',None
